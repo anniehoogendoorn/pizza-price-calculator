7889b3adb022425b0fd7b83100851f9d141a5d8d
Annie <anniehoogendoorn@gmail.com>
2015-09-18 12:06:45 -0700

    Add Pizza.prototype.calculatePrice with spec.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,18 @@ function Pizza(quantity, topping, pizzaSize) {
   this.quantity = quantity;
   this.topping = topping;
   this.pizzaSize = pizzaSize;
+
+};
+
+Pizza.prototype.calculatePrice = function() {
+  var totalPrice = 8 + (this.topping * 1);
+
+  if (this.pizzaSize == "small") {
+    return totalPrice * this.quantity
+  } else if {
+    this.pizzaSize == "medium") {
+      return totalPrice + 2
+    }
+  };
+
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,16 @@
 describe("Pizza", function() {
   debugger;
   it("creates a pizza with the given specifications", function() {
-    var testPizza = new Pizza(1, "olives", "small");
+    var testPizza = new Pizza(1, 1, "small");
     expect(testPizza.quantity).to.equal(1);
-    expect(testPizza.topping).to.equal("olives");
+    expect(testPizza.topping).to.equal(1);
     expect(testPizza.pizzaSize).to.equal("small");
+  });
+});
+
+describe('calculatePrice', function() {
+  it("returns the price for one small pizza with one topping", function() {
+    var testPizza = new Pizza(1, 1, "small");
+    expect(testPizza.calculatePrice()).to.equal(11)
   });
 });
